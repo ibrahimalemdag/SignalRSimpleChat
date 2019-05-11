68566ff672b27809066077f19bc9c20e4513d7ba
ibrahim.alemdag@kafein.com.tr <ibrahim.alemdag@kafein.com.tr>
2019-05-11 18:17:44 +0300

    Revert "Revert "push project""
    This reverts commit 148bce9222e60ab8ef7e9b2a34c4a58e2e7438a4.

---
+++ SsignalRChatApp/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace SsignalRChatApp
+{
+    public class AppDbContext : DbContext
+    {
+        private string mId;
+
+        public String Id
+        {
+            get => mId;
+            set => Guid.NewGuid().ToString("N");
+        }
+
+        public DbSet<SettingDataModel> Settings { get; set; }
+        public DbSet<MessageViewModel> Message { get; set; }
+        public DbSet<MessengerNodeViewModel> Node { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
