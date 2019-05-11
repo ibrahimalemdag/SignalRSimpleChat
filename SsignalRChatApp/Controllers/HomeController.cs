ac048f3f4568f181372f18144542d73a56ea7a2c
ibrahim.alemdag@kafein.com.tr <ibrahim.alemdag@kafein.com.tr>
2019-05-11 17:54:09 +0300

    push project

---
+++ SsignalRChatApp/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SsignalRChatApp.Models;
+using SignalRChat.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SsignalRChatApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        protected AppDbContext mContext;
+        IHubContext<NodeHub, ITypedHubClient> nodeHubContext;
+
+        public HomeController(AppDbContext context, IHubContext<NodeHub, ITypedHubClient> hubContext)
+        {
+            mContext = context;
+            nodeHubContext = hubContext;
+        }
+
+        public IActionResult Index()
+        {
+            mContext.Database.EnsureCreated();
+
+            MessengerNodeViewModel user = new MessengerNodeViewModel() { Value = HttpContext.Connection.Id, TimeStamp = new DateTime() };
+            mContext.Node.Add(user);
+            mContext.SaveChanges();
+
+            // to to broadcatst "i am new" message from server side
+            //nodeHubContext.Clients.All.NotifyMessageToClients(user.Id,HttpContext.Connection.Id);
+
+            ViewData["Title"] = user.Id;
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            ViewData["Title"] = "Chat";
+            ViewData["Message"] = " .net core 2.0 , entitty framework 6.2.0 , signalR 2.4.1";
+            return View();
+        }
+
+      
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add([Bind("Value,SourceNode,TargetNode")] MessageViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                mContext.Message.Add(model);
+            }
+            mContext.SaveChanges();
+
+            return View("Index");
+        }
+    }
+}
---
+++ SsignalRChatApp/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SignalRChat.Hubs;
+
+namespace SsignalRChatApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            String connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+            services.AddMvc();
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        {
+            IocContainer.Provider = (ServiceProvider)serviceProvider;
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chatHub");
+                routes.MapHub<NodeHub>("/NodeHub");
+            });
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
